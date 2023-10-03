9ebbc91fbd91a9b83acb3eebbda448dc7f6b2c82
ippo579 <lengareshreyash@gmail.com>
2023-10-03 15:39:01 +0530

    firts commit

--- src/main/java/com/demo/awscodedeploydemo/controller/apiController.java
+++ src/main/java/com/demo/awscodedeploydemo/controller/apiController.java
@@ -6,11 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class apiController
-{
+public class apiController {
     @GetMapping("api/health")
-    public ResponseEntity<?> healthcheck()
-    {
+    public ResponseEntity<?> healthcheck() {
         return ResponseEntity.ok("It works very Fine.");
     }
+
+
+    @GetMapping("/trigger-error")
+    public String triggerError() {
+        // Intentional runtime error (division by zero)
+        int result = 10 / 0;
+
+        {
+            return "This endpoint triggers a runtime error";
+        }
+    }
 }
+
